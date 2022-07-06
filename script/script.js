a3171b74c615f9a7b29db6eed1780fc12e3d5665
Zé Elias <elias.jpaiva@gmail.com>
2022-07-06 13:03:29 +0200

    added event listeners

--- script/script.js
+++ script/script.js
@@ -9,5 +9,48 @@ const number6 = document.querySelector('.number6');
 const number7 = document.querySelector('.number7');
 const number8 = document.querySelector('.number8');
 const number9 = document.querySelector('.number9');
+const input = document.getElementById('input-calc');
+let accumulater = 0;
 
-//
+//event listeners
+number0.addEventListener('click', ()=> {
+  input.value = number0.innerHTML
+  accumulater = input.value;
+})
+
+number1.addEventListener('click', () => {
+  input.value = number1.innerHTML;
+});
+
+number2.addEventListener('click', () => {
+  input.value = number2.innerHTML;
+});
+
+number3.addEventListener('click', () => {
+  input.value = number3.innerHTML;
+});
+
+number4.addEventListener('click', () => {
+  input.value = number4.innerHTML;
+});
+
+number5.addEventListener('click', () => {
+  input.value = number5.innerHTML;
+});
+
+number6.addEventListener('click', () => {
+  input.value = number6.innerHTML;
+});
+
+number7.addEventListener('click', () => {
+  input.value = number7.innerHTML;
+});
+
+number8.addEventListener('click', () => {
+  input.value = number8.innerHTML;
+});
+
+number9.addEventListener('click', () => {
+  input.value = number9.innerHTML;
+});
+
