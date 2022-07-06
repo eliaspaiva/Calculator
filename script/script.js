a82ac5c7bbbbe341e41a36b22b245e3488104eb4
Zé Elias <elias.jpaiva@gmail.com>
2022-07-06 12:50:59 +0200

    created some variables for the buttons

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
 // Variables
+const number0 = document.querySelector('.number0');
+const number1 = document.querySelector('.number1');
+const number2 = document.querySelector('.number2');
+const number3 = document.querySelector('.number3');
+const number4 = document.querySelector('.number4');
+const number5 = document.querySelector('.number5');
+const number6 = document.querySelector('.number6');
+const number7 = document.querySelector('.number7');
+const number8 = document.querySelector('.number8');
+const number9 = document.querySelector('.number9');
+
+//
