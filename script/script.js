182f4ec842438397b23805716a9c953662b3bb0c
Zé Elias <elias.jpaiva@gmail.com>
2022-07-07 10:33:33 +0200

    testing different logics for the event listeners

--- script/script.js
+++ script/script.js
@@ -18,108 +18,88 @@ let numbersClickedOn = [];
 input.addEventListener('input', () => {
   const numberTypedIn = input.value;
   numbersClickedOn.push(numberTypedIn);
-  console.log(numbersClickedOn);
+  string = numbersClickedOn.join('');
+  input.value = string;
 });
 
 number0.addEventListener('click', ()=> {
   input.value = number0.innerHTML
   const zero = input.value;
   numbersClickedOn.push(zero)
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
   })
 
 number1.addEventListener('click', () => {
   input.value = number1.innerHTML;
   const one = input.value;
   numbersClickedOn.push(one);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number2.addEventListener('click', () => {
   input.value = number2.innerHTML;
   const two = input.value;
   numbersClickedOn.push(two);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number3.addEventListener('click', () => {
   input.value = number3.innerHTML;
   const three = input.value;
   numbersClickedOn.push(three);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number4.addEventListener('click', () => {
   input.value = number4.innerHTML;
   const four = input.value;
   numbersClickedOn.push(four);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number5.addEventListener('click', () => {
   input.value = number5.innerHTML;
   const five = input.value;
   numbersClickedOn.push(five);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  console.log(string);
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number6.addEventListener('click', () => {
   input.value = number6.innerHTML;
   const six = input.value;
   numbersClickedOn.push(six);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number7.addEventListener('click', () => {
   input.value = number7.innerHTML;
   const seven = input.value;
   numbersClickedOn.push(seven);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number8.addEventListener('click', () => {
   input.value = number8.innerHTML;
   const eight = input.value;
   numbersClickedOn.push(eight);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 number9.addEventListener('click', () => {
   input.value = number9.innerHTML;
   const three = input.value;
   numbersClickedOn.push(three);
-  console.log(numbersClickedOn);
   string = numbersClickedOn.join('');
-
-  input.innerHTML = string;
+  input.value = string;
 });
 
 
